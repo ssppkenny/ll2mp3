fa6dc487e65115224f678e8147f1c76b289a85f1
Sergey Mikhno (Galenica) <sergey.mikhno@galenica.com>
2021-12-07 18:34:21 +0100

    converted some files

--- ll2mp3.py
+++ ll2mp3.py
@@ -1,6 +1,8 @@
 import pathlib
 from collections import namedtuple
 import re
+import os
+import eyed3
 
 HEADER_REGEX = r'(PERFORMER) \"?([^\"]*)\"?.*|(TITLE) \"?([^\"]*)\"?.*'
 TRACK_REGEX_TITLE = r'.*?(TRACK\s+(\d+).*?TITLE\s+\"?([^\"+]+)\"?.*?)'
@@ -14,9 +16,9 @@ def parse_cue_file(path):
     d = {}
     with open(path.resolve(), "r") as f:
         text = f.read()
-    file = re.findall(r'FILE \"?(.*)\"? .*\n', text)
+    file = re.findall(r'FILE \"?([^\"]*)\"? .*\n', text)
     file, *_ = file
-    d['file'] = file
+    d['file'] = pathlib.Path(path.parent, file)
     h, t = re.split(r"FILE.*\n", text, re.DOTALL)
     for line in h.split('\n'):
         l = re.findall(HEADER_REGEX, line)
@@ -38,5 +40,42 @@ def find_cue_file(dir_name):
     for cuefile in pathlib.Path(dir_name).glob('*.cue'):
         return cuefile
     return None
+
+def convert(cue_file, cuesheet):
+    audio_file = cuesheet.file.resolve()
+    cmd = f'cuebreakpoints "{cue_file}" | sed s/$/0/ | shnsplit -O always -o flac "{audio_file}"'
+    ret_code = os.system(cmd)
+    if ret_code != 0:
+        convert_to_wav_and_back(audio_file)
+        cmd = f'cuebreakpoints "{cue_file}" | sed s/$/0/ | shnsplit -O always -o flac "{audio_file}.flac"'
+        os.system(cmd)
+
+    rename_files(cuesheet)
+
+def convert_to_wav_and_back(audio_file):
+    cmd = f'ffmpeg -y -i "{audio_file}" "{audio_file}.wav"'
+    os.system(cmd)
+    cmd = f'ffmpeg -y -i "{audio_file}.wav" "{audio_file}.flac"'
+    os.system(cmd)
+
+def rename_files(cuesheet):
+    pd = cuesheet.file.parent.resolve()
+    for track in cuesheet.tracks:
+        src = f"split-track{track.number}.flac"
+        src = f"{pd}/{src}"
+        dst = f"{pd}/{track.title}.mp3"
+        cmd = f'ffmpeg -y -i "{src}" -ab 320k "{dst}"'
+        os.system(cmd)
+        mp3_file = eyed3.load(dst)
+        mp3_file.initTag()
+        mp3_file.tag.artist = cuesheet.performer
+        mp3_file.tag.title = track.title
+        mp3_file.tag.save(dst)
+
+
+
+
+
+
 
 
--- test.py
+++ test.py
@@ -4,11 +4,10 @@ import pathlib
 if __name__ == '__main__':
     p = pathlib.Path('/Users/ugxnbmikhs/code/python/ll2mp3')
     fp = p.resolve()
-    print(fp)
     cuepath = ll2mp3.find_cue_file(fp)
-    ll2mp3.parse_cue_file(cuepath)
-
-
+    cue_file = ll2mp3.parse_cue_file(cuepath)
+    audio_file = cue_file.file
+    ll2mp3.convert(cuepath.resolve(), cue_file)
 
 
 
